6219f83d523d176820c98d4561ee5996ffe5194b
mayankpcp7 <jariwalmayank123@gmail.com>
2024-03-21 16:08:20 +0530

    merge code

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+function shownav() {
+  document.getElementById("mobilenav").classList.add("start_0");
+}
+function hidenav() {
+  document.getElementById("mobilenav").classList.remove("start_0");
+}
