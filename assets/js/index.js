72f282e85d9d4e536964cd086743ebe5677fd158
mayankpcp7 <jariwalmayank123@gmail.com>
2024-03-23 11:48:42 +0530

    merge code

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,7 +8,42 @@ function hidenav() {
   document.getElementById("mobilenav").classList.remove("start_0");
   document.body.style.overflow = "unset";
 }
+//----------------------------- sldier------------------------------------
+$(".responsive").slick({
 
+  infinite: true,
+  speed: 300,
+  infinite: true,
+  slidesToShow: 6,
+  dots: false,
+  slidesToScroll: 1,
+  // autoplay: true,
+  autoplaySpeed: 1000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
 //--------------------------- counter box --------------------------------//
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -51,7 +86,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
-
 
 // //--------------------------- accordion js--------------------------------//
 var acc = document.getElementsByClassName("accordion");
